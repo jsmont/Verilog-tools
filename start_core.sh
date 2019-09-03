b96b8cc968d7572e374f797eb24d5c656565066d
Jordi Solà <jsmont.sol@gmail.com>
2019-09-03 16:42:25 +0200

    Fixing submodule issue

--- start_core.sh
+++ start_core.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-REPOROOT=$1
-CONTAINER_NAME=$2
+COMMAND=$1
+
+pushd $(dirname $0) &>/dev/null
+REPOROOT=$(git rev-parse --show-superproject-working-tree)
+if [ -z "$REPOROOT" ]; then
+    REPOROOT=$(git rev-parse --show-toplevel)
+fi
+echo "REPOROOT=$REPOROOT"
+CONTAINER_NAME=$(basename `git rev-parse --show-toplevel`)
+popd &>/dev/null
 
 export DOCKERTAG="jsola/verilog-tools:latest"
 if [[ "$(docker images -q $DOCKERTAG 2> /dev/null)" == "" ]]; then
@@ -14,3 +22,5 @@ if [[ "$(docker ps -f name=^$CONTAINER_NAME$ | sed -n '1!p')" == "" ]]; then
     echo "Creating container ..."
     docker run -w $REPOROOT --user $(id -u $(whoami)) -i -t -d -v $REPOROOT:$REPOROOT --name=$CONTAINER_NAME $DOCKERTAG > /dev/null
 fi
+
+docker exec -t $CONTAINER_NAME bash -c "$COMMAND"
